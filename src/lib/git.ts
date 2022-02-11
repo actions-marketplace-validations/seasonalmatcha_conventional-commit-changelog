47f3a1769a019e43885ea926b9f237bf7bb4fa88
Seasonal Matcha <seasonalmatcha@gmail.com>
2022-02-11 21:40:37 +0700

    feat(lib): add getRepositoryUrl function

--- src/lib/git.ts
+++ src/lib/git.ts
@@ -6,3 +6,24 @@ const git = simpleGit();
 export const getCommits = async (params: IGetCommitParams) => {
   return git.log(params);
 };
+
+export const getRepositoryUrl = async (): Promise<string | null> => {
+  const {
+    values: {
+      '.git/config': { 'remote.origin.url': remotes },
+    },
+  } = await git.listConfig();
+
+  const remote = Array.isArray(remotes) ? remotes[0] : remotes;
+  const sshRegex = /git@github\.com:(.+)\.git/;
+  const httpRegex = /https:\/\/github\.com\/(.+)/;
+  const replace = 'https://github.com/$1';
+
+  for (const testRegex of [sshRegex, httpRegex]) {
+    if (testRegex.test(remote)) {
+      return remote.replace(sshRegex, replace);
+    }
+  }
+
+  return null;
+};
